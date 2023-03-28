8197783135de7099f45709386dd3f1ce73f85724
Braydon <xcoolmanxx@gmail.com>
2023-03-28 04:57:57 -0400

    oops, forgot api key repo

---
+++ src/main/java/me/braydon/antivpn/repository/mongodb/APIKeyRepository.java
@@ -0,0 +1,34 @@
+package me.braydon.antivpn.repository.mongodb;
+
+import lombok.NonNull;
+import me.braydon.antivpn.model.APIKey;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.UUID;
+
+/**
+ * The {@link APIKey} repository.
+ *
+ * @author Braydon
+ */
+public interface APIKeyRepository extends MongoRepository<APIKey, UUID> {
+    /**
+     * Find the api key object
+     * with the given key.
+     *
+     * @param key the api key
+     * @return the api key, null if none
+     * @see APIKey for api key
+     */
+    @Query("{'key': '?0'}")
+    APIKey findByKey(@NonNull String key);
+    
+    /**
+     * Get the total number of api keys.
+     *
+     * @return the amount of api keys
+     * @see APIKey for api key
+     */
+    long count();
+}
