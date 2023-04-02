4638f37a7ea4f4d173891125c45832cb91f2041b
Braydon <xcoolmanxx@gmail.com>
2023-04-01 22:36:09 -0400

    Add scraping of CloudFlare servers

---
+++ src/main/java/me/braydon/antivpn/provider/impl/CloudflareService.java
@@ -0,0 +1,69 @@
+package me.braydon.antivpn.provider.impl;
+
+import inet.ipaddr.IPAddress;
+import inet.ipaddr.IPAddressString;
+import lombok.SneakyThrows;
+import me.braydon.antivpn.AntiVPN;
+import me.braydon.antivpn.provider.VPNServiceProvider;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This VPN provider is for Cloudflare.
+ *
+ * @author Braydon
+ */
+@Component
+public final class CloudflareService extends VPNServiceProvider {
+    private static final String GET_IPV4_ENDPOINT = "https://www.cloudflare.com/ips-v4"; // Getting IPv4 addresses
+    
+    public CloudflareService() {
+        super("Cloudflare", TimeUnit.DAYS.toMillis(7L));
+    }
+    
+    /**
+     * Initialize this provider.
+     * <p>
+     * This will get the ids of all
+     * regions for this provider.
+     * </p>
+     */
+    @PostConstruct @SneakyThrows
+    public void initialize() {
+        // Add a scrape task to get all provider ips
+        addScrapeTask(new TimedScrapeTask(TimeUnit.DAYS.toMillis(1L), () -> {
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                                          .uri(URI.create(GET_IPV4_ENDPOINT))
+                                          .GET()
+                                          .timeout(Duration.ofSeconds(5L))
+                                          .build();
+                HttpResponse<String> response = AntiVPN.HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() != 200) { // If the status code is not 200
+                    throw new IllegalStateException(String.format("Bad status code (%s) returned", response.statusCode()));
+                }
+                String body = response.body(); // The body of the response
+                for (String range : body.split("\n")) { // Iterate over the returned ranges
+                    Iterator<? extends IPAddress> iterator = new IPAddressString(range).getAddress()
+                                                                 .toPrefixBlock()
+                                                                 .withoutPrefixLength()
+                                                                 .iterator();
+                    while (iterator.hasNext()) {
+                        String ipAddress = iterator.next().toString();
+                        addIp(ipAddress, "IPv4 List"); // Add the IP address
+                    }
+                }
+            } catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }));
+    }
+}
