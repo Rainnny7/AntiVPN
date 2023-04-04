b955a832e692b8ef1825a194e3550d2dc3c259ca
Braydon <xcoolmanxx@gmail.com>
2023-04-03 21:05:19 -0400

    Add provider metrics

---
+++ src/main/java/me/braydon/antivpn/metrics/impl/ProviderTracker.java
@@ -0,0 +1,50 @@
+package me.braydon.antivpn.metrics.impl;
+
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
+import lombok.NonNull;
+import me.braydon.antivpn.metrics.MetricTracker;
+import me.braydon.antivpn.provider.VPNServiceProvider;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Braydon
+ */
+public final class ProviderTracker extends MetricTracker {
+    public ProviderTracker() {
+        super(TimeUnit.SECONDS.toMillis(10L));
+    }
+    
+    /**
+     * Execute this tracker.
+     * <p>
+     * This method will only
+     * be invoked at the given
+     * interval for this tracker.
+     * </p>
+     *
+     * @param chain the chain of points to attach to
+     * @see Point for point
+     */
+    @Override
+    public void track(@NonNull List<Point> chain) {
+        Set<VPNServiceProvider> providers = VPNServiceProvider.getRegistry();
+        
+        // IP Addresses
+        for (VPNServiceProvider provider : providers) {
+            chain.add(Point.measurement("providerIps")
+                          .addTag("provider", provider.getName())
+                          .addField("value", 0) // TODO: impl into cache, too intense to do now
+                          .time(Instant.now().toEpochMilli(), WritePrecision.MS));
+        }
+        
+        // Total provider count
+        chain.add(Point.measurement("totalProviders")
+                      .addField("value", providers.size())
+                      .time(Instant.now().toEpochMilli(), WritePrecision.MS));
+    }
+}
